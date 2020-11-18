73268ad999bcdb9476bc790d2d3669dd40c9c87d
mjakob-gh <root@betablock.fritz.box>
2020-11-18 21:32:47 +0100

    change some things

--- create-jailpkg-package.sh
+++ create-jailpkg-package.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-
+set -x
 if [ -f "jailpkg.plist" ]; then
     rm jailpkg.plist
 fi
 
 PLIST_TMPFILE=$( mktemp /tmp/plist.XXXXXX )
+LANG_TMPFILE=$( mktemp /tmp/lang.XXXXXX )
 
 SRC_DIR="/usr/src"
 REPO_BASE_DIR="/usr/repo/jailpkg"
@@ -29,7 +30,7 @@ fi
 case ${FBSD_VERSION} in
   12*)
     # FreeBSD 12
-    #PLIST_FILES="runtime.plist clibs.plist libexecinfo.plist libucl.plist libfetch.plist libcasper.plist libarchive.plist \
+    #PLIST_FILES="runtime.plist clibs.plist libexecinfo.plist libucl.plist libfetch.plist libcasper.plist libarchive.plist   \
     #liblzma.plist libcrypt.plist libbz2.plist libxo.plist libz.plist libutil.plist at.plist dma.plist"
     PLIST_FILES="at.plist casper.plist clibs.plist dma.plist ee.plist jail.plist lib.plist lib80211.plist libalias.plist     \
                  libarchive.plist libauditd.plist libbe.plist libbegemot.plist libbluetooth.plist libbsdxml.plist            \
@@ -66,12 +67,12 @@ for FILE in ${PLIST_FILES}; do
   cat ${WORLDSTAGE_DIR}/${FILE} >> ${PLIST_TMPFILE}
 done
 
+# get basic language files
+cat ${PLIST_TMPFILE} | grep -E "/usr/share/locale($|/C.UTF-8|/C|/en_US.UTF-8)" >> ${LANG_TMPFILE}
+cat ${PLIST_TMPFILE} | grep -E "/usr/share/nls($|/C.UTF-8|/C|/en_US.UTF-8)"    >> ${LANG_TMPFILE}
+cat ${PLIST_TMPFILE} | grep -E "/usr/share/vi/catalog($|/C|/POSIX|/english)"   >> ${LANG_TMPFILE}
+
 sed -i '' \
-  -e 's#.*/[a-z][a-z]_[A-Z][A-Z]\..*##g'     \
-  -e 's#.*/french..*##g'                     \
-  -e 's#.*/polish..*##g'                     \
-  -e 's#.*/spanish..*##g'                    \
-  -e 's#.*/swedish..*##g'                    \
   -e 's#.*/atf$##g'                          \
   -e 's#.*/atf/.*##g'                        \
   -e 's#.*/boot$##g'                         \
@@ -92,6 +93,8 @@ sed -i '' \
   -e 's#.*ntp.*##g'                          \
   -e 's#.*/pc-sysinstall$##g'                \
   -e 's#.*/pc-sysinstall/.*##g'              \
+  -e 's#.*/etc/kyua$##g'                     \
+  -e 's#.*/etc/kyua/.*##g'                   \
   -e 's#.*/sbin/bectl$##g'                   \
   -e 's#.*/sbin/bsdlabel$##g'                \
   -e 's#.*/sbin/camcontrol$##g'              \
@@ -132,6 +135,7 @@ sed -i '' \
   -e 's#.*/sbin/swapoff$##g'                 \
   -e 's#.*/sbin/swapon$##g'                  \
   -e 's#.*/usr/bin/ar$##g'                   \
+  -e 's#.*/usr/bin/ib.*##g'                  \
   -e 's#.*/usr/bin/bthost$##g'               \
   -e 's#.*/usr/bin/btsockstat$##g'           \
   -e 's#.*/usr/bin/byacc$##g'                \
@@ -211,15 +215,18 @@ sed -i '' \
   -e 's#.*/usr/share/games/.*##g'            \
   -e 's#.*/usr/share/kyua$##g'               \
   -e 's#.*/usr/share/kyua/.*##g'             \
+  -e 's#.*/usr/share/locale/.*##g'           \
+  -e 's#.*/usr/share/locale$##g'             \
   -e 's#.*/usr/share/misc/[a-l].*##g'        \
   -e 's#.*/usr/share/misc/[n-s].*##g'        \
   -e 's#.*/usr/share/misc/[u-z].*##g'        \
   -e 's#.*/usr/share/misc/mail.*##g'         \
   -e 's#.*/usr/share/misc/mdoc.template##g'  \
+  -e 's#.*/usr/share/nls/.*##g'              \
+  -e 's#.*/usr/share/nls$##g'                \
   -e 's#.*/usr/share/syscons/.*##g'          \
   -e 's#.*/usr/share/syscons$##g'            \
-  -e 's#.*/usr/share/vi/catalog/[a-d].*##g'  \
-  -e 's#.*/usr/share/vi/catalog/[f-z].*##g'  \
+  -e 's#.*/usr/share/vi/catalog/.*##g'       \
   -e 's#.*/usr/share/vt$##g'                 \
   -e 's#.*/usr/share/vt/.*##g'               \
   -e 's#.*/usr/tests$##g'                    \
@@ -236,24 +243,24 @@ sed -i '' \
   -e '/^$/d'                                 \
 ${PLIST_TMPFILE}
 
-# add some files back
-for FILE in ${PLIST_FILES}; do
-  cat ${WORLDSTAGE_DIR}/${FILE} | grep -E      \
-      "en_US.UTF-8" \
-  >> ${PLIST_TMPFILE}
-done
+# add language files back
+cat ${LANG_TMPFILE} >> ${PLIST_TMPFILE}
 
 # remove duplicate lines without sorting the file
 awk '! visited[$0]++' ${PLIST_TMPFILE} > jailpkg.plist
 
+# cleanup temp files
 rm ${PLIST_TMPFILE}
+rm ${LANG_TMPFILE}
 
+# create the minijail package
 pkg --option ABI_FILE=${WORLDSTAGE_DIR}/usr/bin/uname --option ALLOW_BASE_SHLIBS=yes create --verbose --timestamp ${SOURCE_DATE_EPOCH} --format ${FORMAT} --level ${LEVEL} --manifest jailpkg.ucl --plist jailpkg.plist --root-dir ${WORLDSTAGE_DIR} --out-dir ${REPO_DIR}
 
+# create the minijail repository
 cd ${WORLDSTAGE_DIR}
 pkg repo --list-files ${REPO_DIR}
 
-# create "latest" symlink
+# create "latest" symlink in repository
 echo "Creating symlink latest -> ${FBSD_VERSION}.r${REVISION}"
 cd ${REPO_DIR}/..
 rm latest
